4f68d920c84ad66a98d8ea97a96b52aa546bac1b
Sean Roberts <seanroberts@berkeley.edu>
2016-04-04 15:50:52 -0700

    Split table mode tests added for logic block

--- lab3/src/main/scala/logic_block.scala
+++ lab3/src/main/scala/logic_block.scala
@@ -283,6 +283,45 @@ class LogicBlockModuleTests(c: LogicBlockModule) extends Tester(c) {
 
 
   println("\n=====Split Table Mode Tests=====")
+  encoding = BigInt(0x0000000000000000L)
+  poke(c.io.config, encoding)
+  // Stet to split table mode
+  encoding = replace_range(encoding, 0x1, 3, 13)
+  // Set A to be H wire pair 1 above and set A'
+  encoding = replace_range(encoding, 0xA6, 8, 56)
+  // Set B to be H wire pair 2 below and set B'
+  encoding = replace_range(encoding, 0xE2, 8, 48)
+  // Set C to be G wire pair 3 above and set C'
+  encoding = replace_range(encoding, 0xB2, 8, 40)
+  // Set D to be V wire pair 0 and set mx
+  encoding = replace_range(encoding, 0x7D, 8, 32)
+
+  for (trial <- 1 to TRIALS) {
+    println("--Trial " + trial.toString + "--")
+    var inputs = new Array[Int](4)
+    for (i <- 0 until 4) {
+      inputs(i) = rand.nextInt(4)
+    }
+    println("Random Inputs: " + inputs.mkString(", "))
+    var table = rand.nextInt(0x10000)
+    var big_table = BigInt(table)
+    println("Random Table: 0x" + big_table.toString(16))
+    encoding = replace_range(encoding, table, 16, 16)
+    println("Encoding: 0x" + encoding.toString(16))
+    poke(c.io.config, encoding)
+    poke(c.io.H_wire_above_in(1), inputs(0))
+    poke(c.io.H_wire_below_in(2), inputs(1))
+    poke(c.io.G_wire_above_in(3), inputs(2))
+    poke(c.io.V_wire_in(0), inputs(3))
+    // Expect D' be 10 as seen by functional unit.
+    var upper_addr = 1 * (inputs(0) >> 1) + 2 * (inputs(1) >> 1) + 4 * (inputs(2) >> 1) + 8 * (1)
+    var lower_addr = 1 * (inputs(0) & 1) + 2 * (inputs(1) & 1) + 4 * (inputs(2) & 1) + 8 * (0)
+    var upper_result = if(big_table.testBit(upper_addr)) 1 else 0
+    var lower_result = if(big_table.testBit(lower_addr)) 1 else 0
+    expect(c.io.H_wire_out, (upper_result << 1) | lower_result)
+    println("")
+  }
+
 
   println("\n=====Select Mode Tests=====")
 
