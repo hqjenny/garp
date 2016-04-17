d358c733b592158d89031c374001db1bf580f124
Sean Roberts <seanroberts@berkeley.edu>
2016-04-17 16:28:25 -0700

    Vwire module generates all connections but needs blackboxes and interface to be matched to garp array

--- lab3/src/main/scala/vwire.scala
+++ lab3/src/main/scala/vwire.scala
@@ -118,23 +118,82 @@ class Vwire(val N: Int, val W: Int=2) extends Module {
   io.out := VwireBB.out
 }
 
-class VwireModule(val R: Int = 32, val C: Int = 23, val W: Int = 2) extends Module {
+object calc_base_index {
+  def apply(nom_len:Int, nom_first:Int) : Int = {
+    if (nom_len == 4) {
+      return 2*(((nom_first%2)+2)%2)
+    } else {
+      return 2*(BigInt(nom_len).bitLength - 2)
+    }
+  }
+}
+
+object shift_index {
+  def apply(nom_len:Int, nom_first:Int, row:Int) : Int = {
+    val fourth = nom_len / 4
+    if(row < (nom_first + fourth) || row >= (nom_first + 3 * fourth)) {
+      return 0
+    } else {
+      return 1
+    }
+  }
+}
+
+class VwireModule(val R: Int = 8, val C: Int = 23, val V: Int = 16, val W: Int = 2) extends Module {
   val io = new Bundle { 
-    val VwirePorts  = Vec.fill(R){Vec.fill(C){new BusPort(W)}}
+    val VwirePorts  = Vec.fill(R){Vec.fill(C){Vec.fill(V){new BusPort(W)}}}
   }
 
-  val wire_consts = VWIRECONSTANTS.vwire_32
-  for (row <- wire_consts) {
-    println(row(0))
-    println(row.slice(1, row.length).deep.mkString(","))
+  val printout = true
+
+  val wire_consts = R match {
+    case 4 => VWIRECONSTANTS.vwire_4
+    case 8 => VWIRECONSTANTS.vwire_8
+    case 16 => VWIRECONSTANTS.vwire_16
+    case 32 => VWIRECONSTANTS.vwire_32
+    case 64 => VWIRECONSTANTS.vwire_64
+    case 128 => VWIRECONSTANTS.vwire_128
+    case 256 => VWIRECONSTANTS.vwire_256
+    case _ => VWIRECONSTANTS.vwire_256 //Default to largest set, will probably result in errors.
   }
-  io.VwirePorts(30)(20).out := io.VwirePorts(31)(21).in
-  val wirea = Module(new Vwire(2))
-  val wireb = Module(new Vwire(3))
-  val wirec = Module(new Vwire(8))
+
+  for (i <- 0 until wire_consts.length) {
+    val wire_bundle = wire_consts(i).asInstanceOf[Array[Any]]
+    val actual_len = wire_bundle(0).asInstanceOf[Int]
+    if (printout) {
+      println("Generate Vwires with actual length " + actual_len)
+    }
+    for (j <- 1 until wire_bundle.length) {
+      val wire = wire_bundle(j).asInstanceOf[Array[Int]]
+      val nom_len = wire(0)
+      val nom_first = wire(1)
+      val first = wire(2)
+      val last = wire(3)
+      val base_index = calc_base_index(nom_len, nom_first)
+      if (printout) {
+        println("\tGenerate specific Vwire with properties {nom_len: " + nom_len + " nom_first: " + nom_first + " first: " + first + " last: " + last + "}")
+      }
+      //Generate C copies of this wire to add to each column
+      val current_Vwires = Vec.fill(C){Module(new Vwire(actual_len)).io}
+      for (row <- first to last) {
+        // Calculate which index this wire should connect to in the logic block
+        val vwire_index = base_index + shift_index(nom_len, nom_first, row)
+        val current_VwirePorts = io.VwirePorts(row)
+        if (printout) {
+          println("\t\t Connecting Vwire port " + (row - first) + " to array port in row " + row + " at index " + vwire_index)
+        }
+        for (k <- 0 until C) {
+          current_Vwires(k).in(row - first) := current_VwirePorts(k)(vwire_index).in
+          current_Vwires(k).en(row - first) := current_VwirePorts(k)(vwire_index).en
+          current_VwirePorts(k)(vwire_index).out := current_Vwires(k).out
+        }
+      }
+    }
+  }
 }
 
 class VwireModuleTests(c: VwireModule) extends Tester(c) {
+      /*
       poke(c.io.VwirePorts(31)(21).in, 0)
       peek(c.io.VwirePorts(30)(20).out)
       peek(c.io.VwirePorts(31)(21).out)
@@ -142,6 +201,7 @@ class VwireModuleTests(c: VwireModule) extends Tester(c) {
       poke(c.io.VwirePorts(31)(21).in, 2)
       peek(c.io.VwirePorts(30)(20).out)
       peek(c.io.VwirePorts(31)(21).out)
+      */
 }
 
 object VwireModuleMain { 
